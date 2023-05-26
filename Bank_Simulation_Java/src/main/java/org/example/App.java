a4a3c8ebd1619c73341c91aaa4a48e1823ff4a4e
JustynaGajdek <131193212+JustynaGajdek@users.noreply.github.com>
2023-05-26 11:52:21 +0200

    third commit - small fixes

--- Bank_Simulation_Java/src/main/java/org/example/App.java
+++ Bank_Simulation_Java/src/main/java/org/example/App.java
@@ -4,28 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-// Napisz program, który będzie symulować działanie banku. Zaimplementuj klasy:
-//a. Klasę Client, reprezentującą klienta banku. Klasa powinna zawierać pola:
-// name (typu String), accountNumber (typu String) oraz balance (typu double chyba że dasz radę inaczej),
-// reprezentujące odpowiednio imię i nazwisko klienta, numer konta oraz saldo.
-//b. Klasę Bank, reprezentującą bank. Klasa powinna zawierać pole clients, będące tablicą obiektów typu Client,
-// oraz metody:
-//
-//addClient(Client client): dodająca nowego klienta do tablicy clients,
-//removeClient(String accountNumber): usuwająca klienta o podanym numerze konta z tablicy clients,
-//getClientCount(): zwracająca liczbę klientów w banku,
-//getClientByAccountNumber(String accountNumber): zwracająca obiekt klienta o podanym numerze konta,
-//deposit(String accountNumber, double amount): dokonująca wpłaty na konto klienta o podanym numerze konta,
-//withdraw(String accountNumber, double amount): dokonująca wypłaty z konta klienta o podanym numerze konta,
-//transfer(String fromAccountNumber, String toAccountNumber, double amount): dokonująca przelewu z jednego
-// konta na drugie.
-//c. Klasę Main, w której znajdzie się metoda main, a w niej kod, w którym:
-//i. Stworzysz obiekt banku.
-//ii. Dodasz kilku klientów do banku, korzystając z metody addClient.
-//iii. Wywołasz odpowiednie metody, aby dokonać wpłat, wypłat i przelewów między kontami klientów.
-//iv. Wypiszesz na ekranie informacje o wszystkich klientach w banku.
-//Twoim zadaniem jest implementacja klas Client, Bank oraz metody main zgodnie z podanymi wymaganiami.
-
         Bank bank;
         bank = new Bank();
 
